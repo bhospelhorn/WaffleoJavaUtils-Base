f7659590b27e534ea54bd0011552f0e72695f7e3
Blythe Hospelhorn <35576053+bhospelhorn@users.noreply.github.com>
2021-02-08 16:11:29 -0600

    Added colorspace enums to VideoIO

--- src/waffleoRai_Video/VideoIO.java
+++ src/waffleoRai_Video/VideoIO.java
@@ -2,11 +2,17 @@ package waffleoRai_Video;
 
 public class VideoIO {
 
-	public static final int CLRFMT_ARGB8_STD = 1;
-	public static final int CLRFMT_RGB8_STD = 2;
+	public static final int CLRFMT_ARGB8_BYPIX = 1;
+	public static final int CLRFMT_RGB8_BYPIX = 2;
 	public static final int CLRFMT_YUV420 = 3;
 	public static final int CLRFMT_YUV422 = 4;
 	public static final int CLRFMT_YUV444 = 5;
 	public static final int CLRFMT_RGB444 = 6;
+	
+	public static final int CLRSPACE_ARGB8 = 1;
+	public static final int CLRSPACE_RGB8 = 2;
+	public static final int CLRSPACE_YUV_SD = 3;
+	public static final int CLRSPACE_YUV_HD = 4;
+	public static final int CLRSPACE_YUV_2020 = 5;
 	
 }
