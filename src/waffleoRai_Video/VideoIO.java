8ef5366a66caabe93c513953220d14a34c44b23b
Blythe Hospelhorn <35576053+bhospelhorn@users.noreply.github.com>
2021-02-16 14:20:43 -0600

    Video debugging for mkv adapter

--- src/waffleoRai_Video/VideoIO.java
+++ src/waffleoRai_Video/VideoIO.java
@@ -8,6 +8,9 @@ public class VideoIO {
 	public static final int CLRFMT_YUV422 = 4;
 	public static final int CLRFMT_YUV444 = 5;
 	public static final int CLRFMT_RGB444 = 6;
+	public static final int CLRFMT_YUV420_16LE = 7;
+	public static final int CLRFMT_YUV422_16LE = 8;
+	public static final int CLRFMT_YUV444_16LE = 9;
 	
 	public static final int CLRSPACE_ARGB8 = 1;
 	public static final int CLRSPACE_RGB8 = 2;
